5ee62b5f70d0a2f145b74e33440a2a50eb76c2da
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-25 21:22:11 -0400

    commit tree c655200429d6993a15b1f956154169ffa7a38aa0

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_data)
+    if search_data.present?
+      where('NAME like ?', "%#{search_data}%")
+    else
+      self.all
+    end
+  end
 end
